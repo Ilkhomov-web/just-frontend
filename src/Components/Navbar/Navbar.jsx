1ace417bcbbb0537d42b66d1ec203f1feba2241a
Elyor <ilhomovelyor20010624@gmail.com>
2024-05-06 17:43:54 +0500

    some change

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,24 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css';
 import logo from '../../assets/logo.png';
 import card_icon from '../../assets/cart_icon.png';
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+  const [menu, setMenu] = useState("Shop")
+
   return (
     <div className='navbar'> 
-      <div className="card-logo">
+      <div className="nav-logo">
         <img src={logo} alt="" />
         <p>Just Shop</p>
       </div>
       <ul className="nav-menu">
-        <li>Shop</li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>Kids</li>
+        <li onClick={() => {setMenu("shop")}}><Link style={{textDecoration: 'none'}} to="/">Shop</Link>{menu === "shop" ? <hr /> : <></>}</li>
+        <li onClick={() => {setMenu("mens")}}><Link style={{textDecoration: 'none'}} to="/mens">Men</Link>{menu === "mens" ? <hr /> : <></>}</li>
+        <li onClick={() => {setMenu("womens")}}><Link style={{textDecoration: 'none'}} to="/womens">Women</Link>{menu === "womens" ? <hr /> : <></>}</li>
+        <li onClick={() => {setMenu("kids")}}><Link style={{textDecoration: 'none'}} to="/kids">Kids</Link>{menu === "kids" ? <hr /> : <></>}</li>
       </ul>
       <div className="nav-login-card">
-        <button>Login</button>
-        <img src={card_icon} alt="" />
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/card">
+          <img src={card_icon} alt="" />
+        </Link>
+        <div className="nav-card-count">0</div>
       </div>
     </div>
   )
