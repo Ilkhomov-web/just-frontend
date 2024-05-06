1f071cdb5d6892c4ef0854cb8b70f6a316de2e20
Elyor <ilhomovelyor20010624@gmail.com>
2024-05-06 13:28:29 +0500

    some change

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
-import './Navbar.css'
+import './Navbar.css';
+import logo from '../../assets/logo.png';
+import card_icon from '../../assets/cart_icon.png';
 
 const Navbar = () => {
   return (
-    <div>
-      hello
+    <div className='navbar'> 
+      <div className="card-logo">
+        <img src={logo} alt="" />
+        <p>Just Shop</p>
+      </div>
+      <ul className="nav-menu">
+        <li>Shop</li>
+        <li>Men</li>
+        <li>Women</li>
+        <li>Kids</li>
+      </ul>
+      <div className="nav-login-card">
+        <button>Login</button>
+        <img src={card_icon} alt="" />
+      </div>
     </div>
   )
 }
