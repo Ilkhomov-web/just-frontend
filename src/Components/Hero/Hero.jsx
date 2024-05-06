803f24c74e72373eb73c7bafdfcaa5898b97627f
Elyor <ilhomovelyor20010624@gmail.com>
2024-05-06 18:27:20 +0500

    some change

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
-import './Hero.css'
+import './Hero.css';
+import hand_icon from '../../assets/hand_icon.png'
+import arrow_icon from '../../assets/arrow.png'
 
 const Hero = () => {
   return (
     <div className='hero'>
-      
+      <div className="hero-left">
+        <h2>New Arrivals only</h2>
+        <div>
+          <div className="hand-hand-icon">
+            <p>new</p>
+            <img src={hand_icon} alt="" />
+          </div>
+          <p>collactions</p>
+          <p>for everyone</p>
+        </div>
+        <div className="hero-lates-btn">
+          <div>Latest Collaction</div>
+          <img src={arrow_icon} alt="" />
+        </div>
+      </div>
+      <div className="hero-right">
+
+      </div>
     </div>
   )
 }
