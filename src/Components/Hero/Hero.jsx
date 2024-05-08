ff61e2b2c91de5c413f2b2428753137f563934f8
Elyor <ilhomovelyor20010624@gmail.com>
2024-05-08 11:08:30 +0500

    some change

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Hero.css';
 import hand_icon from '../../assets/hand_icon.png'
 import arrow_icon from '../../assets/arrow.png'
+import hero_image from '../../assets/hero_image.png'
 
 const Hero = () => {
   return (
@@ -9,7 +10,7 @@ const Hero = () => {
       <div className="hero-left">
         <h2>New Arrivals only</h2>
         <div>
-          <div className="hand-hand-icon">
+          <div className="hero-hand-icon">
             <p>new</p>
             <img src={hand_icon} alt="" />
           </div>
@@ -22,10 +23,11 @@ const Hero = () => {
         </div>
       </div>
       <div className="hero-right">
-
+        <img src={hero_image} alt="" />
       </div>
     </div>
   )
 }
 
 export default Hero
+
