e331c2095f219f26401858b1137ffe189c4a91f2
Elyor <ilhomovelyor20010624@gmail.com>
2024-05-16 13:10:27 +0500

    some change

--- src/Components/NewLetter/NewLetter.jsx
+++ src/Components/NewLetter/NewLetter.jsx
@@ -5,7 +5,7 @@ const NewsLetter = () => {
   return (
     <div className='newsLetter'>
       <h1>Get Exclusive Offers On Your Email</h1>
-      <p>Subscribe to our newletter and stay updated</p>
+      <p className='p'>Subscribe to our newletter and stay updated</p>
       <div>
         <input type="email" placeholder='Your Email Id' />
         <button>Subscribe</button>
