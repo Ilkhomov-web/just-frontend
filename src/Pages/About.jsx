d70a9fb29ad7582e11ccdf347ab93c02f403085e
Ilkhomov-web <ilhomovelyor119@gmail.com>
2025-03-09 22:54:28 +0300

    some change
    some chnage

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,34 +1,29 @@
-import * as React from 'react';
-import { useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MobileStepper from '@mui/material/MobileStepper';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import {KeyboardArrowLeft, KeyboardArrowRight} from '@mui/icons-material';
-import SwipeableViews from 'react-swipeable-views';
-import { autoPlay } from 'react-swipeable-views-utils';
-import AccordionUsage from '../Components/Accardion/Accardion';
-import jonka from '../assets/jonka.webp';
-import nikeK from '../assets/justDoIt.jpeg'
-import just from '../assets/kras.webp'
-
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+import * as React from "react";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MobileStepper from "@mui/material/MobileStepper";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { KeyboardArrowLeft, KeyboardArrowRight } from "@mui/icons-material";
+import AccordionUsage from "../Components/Accardion/Accardion";
+import jonka from "../assets/jonka.webp";
+import nikeK from "../assets/justDoIt.jpeg";
+import just from "../assets/kras.webp";
 
 const images = [
   {
-    label: 'Just Shop',
+    label: "Just Shop",
     imgPath: jonka,
   },
   {
-    label: 'Just Shop',
+    label: "Just Shop",
     imgPath: nikeK,
   },
   {
-    label: 'Just Shop',
+    label: "Just Shop",
     imgPath: just,
   },
-  
 ];
 
 function About() {
@@ -44,50 +39,33 @@ function About() {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
 
-  const handleStepChange = (step) => {
-    setActiveStep(step);
-  };
-
   return (
-    <Box sx={{ maxWidth: '100%', flexGrow: 1}}>
+    <Box sx={{ maxWidth: "100%", flexGrow: 1 }}>
       <Paper
         square
         elevation={0}
         sx={{
-          display: 'flex',
-          alignItems: 'center',
+          display: "flex",
+          alignItems: "center",
           height: 50,
           pl: 2,
-          bgcolor: 'background.default',
+          bgcolor: "background.default",
         }}
       >
         <Typography>{images[activeStep].label}</Typography>
       </Paper>
-      <AutoPlaySwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-        index={activeStep}
-        onChangeIndex={handleStepChange}
-        enableMouseEvents
-      >
-        {images.map((step, index) => (
-          <div key={step.label}>
-            {Math.abs(activeStep - index) <= 2 ? (
-              <Box
-                component="img"
-                sx={{
-                  height: 355,
-                  display: 'block',
-                  maxWidth: '100%',
-                  overflow: 'hidden',
-                  width: '100%',
-                }}
-                src={step.imgPath}
-                alt={step.label}
-              />
-            ) : null}
-          </div>
-        ))}
-      </AutoPlaySwipeableViews>
+      <Box
+        component="img"
+        sx={{
+          height: 355,
+          display: "block",
+          maxWidth: "100%",
+          overflow: "hidden",
+          width: "100%",
+        }}
+        src={images[activeStep].imgPath}
+        alt={images[activeStep].label}
+      />
       <MobileStepper
         steps={maxSteps}
         position="static"
@@ -99,7 +77,7 @@ function About() {
             disabled={activeStep === maxSteps - 1}
           >
             Next
-            {theme.direction === 'rtl' ? (
+            {theme.direction === "rtl" ? (
               <KeyboardArrowLeft />
             ) : (
               <KeyboardArrowRight />
@@ -108,7 +86,7 @@ function About() {
         }
         backButton={
           <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-            {theme.direction === 'rtl' ? (
+            {theme.direction === "rtl" ? (
               <KeyboardArrowRight />
             ) : (
               <KeyboardArrowLeft />
@@ -117,19 +95,18 @@ function About() {
           </Button>
         }
       />
-      <Box sx={{
-        width: '90%',
-        margin: '0 auto'
-      }}>
-        <Box sx={{
-          width: '100%',
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          padding: '20px 0px'
-        }}>
-          <Typography variant='h3'>Just Shop</Typography>
-          <Typography variant='h4'>Savollarga Javob</Typography>
+      <Box sx={{ width: "90%", margin: "0 auto" }}>
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            padding: "20px 0px",
+          }}
+        >
+          <Typography variant="h3">Just Shop</Typography>
+          <Typography variant="h4">Savollarga Javob</Typography>
         </Box>
         <AccordionUsage />
       </Box>
